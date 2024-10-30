9ac655852e4dc52d724fb22fd7851a276df57307
Szymon-Winiecki <szymon.win4@gmail.com>
2024-10-30 15:38:40 +0100

    save information about summarization process in generated files

--- summarize.py
+++ summarize.py
@@ -2,6 +2,7 @@ import ollama
 import pathlib
 import argparse
 import json
+import datetime
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Summarize the job offer')
@@ -12,31 +13,51 @@ def parse_args():
     return parser.parse_args()
 
 def summarize_offer(prompt, offer, model, offer_placeholder="${OFFER}", verbose=False):
+
     prompt = prompt.replace(offer_placeholder, offer)
-    response = ""
+
     stream = ollama.generate(
         model=model,
         prompt=prompt,
         stream=True,
     )
 
+    info = {}
+    response = ""
     for chunk in stream:
         response += chunk['response']
         if verbose:
             print(chunk['response'], end='', flush=True)
+        
+        if chunk['done']:
+            info['total_duration'] = chunk['total_duration']
+            info['load_duration'] = chunk['load_duration']
+            info['prompt_eval_count'] = chunk['prompt_eval_count']
+            info['prompt_eval_duration'] = chunk['prompt_eval_duration']
+            info['eval_count'] = chunk['eval_count']
+            info['eval_duration'] = chunk['eval_duration']
 
-    return response
+    return {
+            "response": response, 
+            "info": info ,
+            }
 
 def handle_summarization(prompt_path, offer_path, output_path, model, offer_placeholder="${OFFER}", verbose=False):
     prompt = open(prompt_path, 'r').read()
     offer = open(offer_path, 'r').read()
 
-    response = summarize_offer(prompt, offer, model, offer_placeholder, verbose)
+    summarization_result = summarize_offer(prompt, offer, model, offer_placeholder, verbose)
 
-    response = json.loads(response)
+    response = json.loads(summarization_result["response"])
 
     result = {
-        'offer_summary': response
+        'offer_summary': response,
+        'info': summarization_result['info'],
+        'model': model,
+        'prompt_path': prompt_path,
+        'offer_path': offer_path,
+        'timestamp' : datetime.datetime.now().isoformat(' '),
+        'LLM_engine': 'ollama',
     }
 
     pathlib.Path(output_path).parent.mkdir(parents=True, exist_ok=True)
